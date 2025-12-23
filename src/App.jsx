b986d8e9c45244d44424768c7dd487d2994466a7
baqar-abbas <baqar.abbas@hotmail.com>
2025-12-23 20:10:08 +0500

    develop responsive header with navigation and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <h1>Hello, React + TailwindCSS world!</h1>
-      </div>
-    </>
+    <div className="min-h-screen bg-dark text-white">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        {/* All other routes go to NotFound */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 
