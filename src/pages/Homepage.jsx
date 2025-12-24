9e33a9bc3a1e21db60941fb4b2bc0c7dcc44d621
baqar-abbas <baqar.abbas@hotmail.com>
2025-12-24 10:47:24 +0500

    develop setapp carousal as per the Figma design

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import Hero from "../components/Hero";
 import FeaturesGrid from "../components/FeaturesGrid";
 import TestimonialCarousel from "../components/TestimonialCarousel";
+import SetappCarousal from "../components/SetappCarousal";
 
 const Homepage = () => {
   return (
@@ -8,6 +9,7 @@ const Homepage = () => {
       <Hero />
       <FeaturesGrid />
       <TestimonialCarousel />
+      <SetappCarousal />
     </main>
   );
 };
