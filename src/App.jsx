634e944698e772a20431f7c4345c75c38eeef1a4
baqar-abbas <baqar.abbas@hotmail.com>
2025-12-24 13:19:56 +0500

    Develop Footer and scroll to Top component

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
 import NotFound from "./pages/NotFound";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div className="min-h-screen bg-dark text-white">
       <Header />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Homepage />} />
         {/* All other routes go to NotFound */}
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
